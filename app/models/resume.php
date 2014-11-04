3b7ac0afa84503d9d8f1cef5397d19c65e4a66da
snoozie <stheron08@gmail.com>
2014-11-05 10:28:02 +1100

    Added comments
    Added comments to the model, resume.php.

--- app/models/resume.php
+++ app/models/resume.php
@@ -1,46 +1,64 @@
 <?php namespace models;
 class Resume extends \core\model{
 
-	/**
-	 * call the parent construct
-	 */
-	public function __construct(){
-		parent::__construct();
-	}
-
+    /**
+     * call the parent construct
+     */
+    public function __construct(){
+            parent::__construct();
+    }
+    
+    /*
+     * Get personal details from database
+     */
     public function getPersonalDetails()
     {
         return $this->_db->select("SELECT * FROM `personal details`");
     }
     
+    /*
+     * Get objectives from database
+     */
     public function getObjectives()
     {
         return $this->_db->select("SELECT * FROM `objectives`");
     }
     
+    /*
+     * Get skills from database
+     */
     public function getSkills()
     {
-        $skills = array();
-        $rows = $this->_db->select("SELECT * FROM `skills`");
-        
-        return $rows;
+        return $this->_db->select("SELECT * FROM `skills`");
     }
     
+    /*
+     * Get education from database
+     */
     public function getEducation()
     {
         return $this->_db->select("SELECT * FROM `education`");
     }
     
+    /*
+     * Get experience from database
+     */
     public function getExperience()
     {
         return $this->_db->select("SELECT * FROM `experience` Order by `Start` DESC");
     }
     
+    /*
+     * Get hobbies from database
+     */
     public function getHobbies()
     {
         return $this->_db->select("SELECT * FROM `hobbies`");
     }
     
+    /*
+     * Get references from database
+     */
     public function getReferences()
     {
         return $this->_db->select("SELECT * FROM `references`");
